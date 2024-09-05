5f27cbf603b137b1574fa723a79be982448e1a4e
Anderson <andersonbonde@gmail.com>
2024-09-05 11:59:30 -0300

    Add index page scaffold

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,12 @@
+const db = require('../database/queries');
+
 const index = async (req, res) => {
-  res.send('Index WIP');
+  const messages = await db.getAllMessages();
+
+  res.render('index', {
+    title: 'Homepage',
+    messages: messages,
+  })
 };
 
 module.exports = {
--- database/queries.js
+++ database/queries.js
@@ -1,7 +1,7 @@
 const pool = require('./pool');
 
 async function getAllMessages() {
-  const { rows } = pool.query('SELECT messages.title, messages.content, messages.timestamp, users.firstname, users.lastname, users.email, users.membership, users.admin FROM messages INNER JOIN users ON messages.user_id = users.id');
+  const { rows } = await pool.query('SELECT messages.title, messages.content, messages.timestamp, users.firstname, users.lastname, users.email, users.membership, users.admin FROM messages INNER JOIN users ON messages.user_id = users.id');
 
   return rows;
 };
